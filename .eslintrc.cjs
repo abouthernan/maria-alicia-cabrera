85b0d28dc7ba83169db953af40959556e92a436b
Cristhian Melo <rxtsel@hotmail.com>
2023-03-16 15:06:00 -0500

    chore: add rule of ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,7 @@ module.exports = {
   ],
   rules: {
     'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/key-spacing': 'off'
   }
 }
