a362a287359f1dd4e4c10a1ecf57fd948c0f5871
Cristhian Melo <rxtsel@hotmail.com>
2023-03-27 19:28:39 -0500

    chore: add sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import sitemap from "@astrojs/sitemap";
 
-import react from '@astrojs/react'
-
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   site: 'https://mariaaliciacabrera.com',
   base: '/',
   trailingSlash: 'never'
-})
+});
