93265f9eb857544833756141c97f4847011c48f2
Cristhian Melo <rxtsel@hotmail.com>
2023-03-29 10:30:46 -0500

    chore: add vercel for ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), sitemap()],
   site: 'https://mariaaliciacabrera.com',
   base: '/',
-  trailingSlash: 'never'
-});
+  trailingSlash: 'never',
+  output: 'server',
+  adapter: vercel()
+})
