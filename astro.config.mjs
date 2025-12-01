b522f5ab7e1f6701faa5500a793abcc41965560d
abouthernan <89414181+abouthernan@users.noreply.github.com>
2025-12-01 13:02:07 -0500

    Change output type to static in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
-import vercel from '@astrojs/vercel/serverless'
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react(), sitemap()],
   site: 'https://mariaaliciacabrera.com',
   base: '/',
   trailingSlash: 'never',
-  output: 'server',
-  adapter: vercel()
+  output: 'static'
 })
